9b37b0a4766864f96adb4ec33c944d8ad754358a
Andreas Schoeper <andreas.schoepfer@erni.ch>
2015-02-02 14:25:48 +0100

    added submodul RamUtils

--- src/arduino_6dof_test.cpp
+++ src/arduino_6dof_test.cpp
@@ -6,7 +6,10 @@
 #include "Timer.h"
 #include "CommunicationUtils.h"
 #include "FreeSixIMU.h"
+#include "RamUtils.h"
 #include <Wire.h>
+
+#define DEBUG_RAM 1  //Printing free Ram space with serial monitor
 
 float q[4]; //hold q values
 
@@ -15,6 +18,17 @@ FreeSixIMU* my3IMU = 0;
 
 Mmi* mmi = 0;
 AMmiAdapter* aMmiAdapter = 0;
+
+Timer* ramDebugTimer = 0;
+const unsigned int c_ramDbgInterval = 10000;
+class RamDebugTimerAdapter : public TimerAdapter
+{
+  void timeExpired()
+  {
+    Serial.print("Free RAM: ");
+    Serial.println(RamUtils::getFreeRam(), DEC);
+  }
+};
 
 //The setup function is called once at startup of the sketch
 void setup()
@@ -30,6 +44,13 @@ void setup()
   delay(5);
   my3IMU->init(); //begin the IMU
   delay(5);
+
+#if DEBUG_RAM
+  // Print free RAM periodically
+  Serial.print("Free RAM: ");
+  Serial.println(RamUtils::getFreeRam(), DEC);
+  ramDebugTimer = new Timer(new RamDebugTimerAdapter(), Timer::IS_RECURRING, c_ramDbgInterval);
+#endif
 }
 
 // The loop function is called in an endless loop
